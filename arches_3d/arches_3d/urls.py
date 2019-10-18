0912ef9458d530d719a10f5f89d9d24473892afd
Vincent Meijer <meijer.vincent@gmail.com>
2019-10-18 14:44:07 +0200

    #156 Override signup url (returning 404 allows less overriding of base Arches code)

--- arches_3d/arches_3d/urls.py
+++ arches_3d/arches_3d/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from arches_3d.views import brochure
-
-
+from arches_3d.views.auth import SignupView, ConfirmSignupView
 from arches_3d.views import projects, heritage_sites, three_d_models, meta_data
 
+
 urlpatterns = [
-    url(r'^', include('arches.urls')),
     url(r'^projects$', projects.ProjectsView.as_view(), name="projects"),
     url(r'^sites$', heritage_sites.HeritageSitesView.as_view(), name="sites"),
     url(r'^3d-models$', three_d_models.ThreeDModelsView.as_view(), name="three_d_models"),
@@ -16,7 +15,10 @@ urlpatterns = [
     url(r'^equipment/', brochure.equipment, name='equipment'),
     url(r'^news/', brochure.news, name='news'),
     url(r'^publications/', brochure.publications, name='publications'),
-    url(r'^labs/', brochure.labs, name='labs')
+    url(r'^labs/', brochure.labs, name='labs'),
+    url(r'^auth/signup$', SignupView.as_view(), name='signup'),
+    url(r'^auth/confirm_signup$', ConfirmSignupView.as_view(), name='confirm_signup'),
+    url(r'^', include('arches.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
